0aff0cf38a546472a8784f071ee3b83c070c3fe3
ChristianKreuzberger <christian.kreuzberger@itec.aau.at>
2015-09-03 09:16:21 +0200

    added methods for exporting matrix to mathematica and matlab

--- math_network.py
+++ math_network.py
@@ -9,6 +9,7 @@ __status__= 'Development'
 
 
 import math
+import numpy as np
 
 
 def c_sum_flows(flows):
@@ -23,3 +24,48 @@ def c_neg_log_sum_flows(flows):
 def c_grad_neg_log_sum_flows(flows):
     return [-1/xk for xk in flows]
 
+
+def np_array_to_mathematica(arr,nfor='{:1.0f}'):
+    """
+    :type arr: np.array
+    :param arr: the array that should be converted to a mathematica string
+    :return: the mathematica string
+    """
+    arr = np.array(arr)
+
+    if arr.ndim == 2:
+        rowlist = []
+
+        for row in arr:
+            a = ",".join([nfor.format(i) for i in row])
+            rowlist.append( "{" + a + "}")
+
+        matrix = ",\n".join(rowlist)
+
+        return "{" + matrix + "}"
+    else: # arr.ndim == 1
+        a = ",".join([nfor.format(i) for i in arr])
+        return "{" + a + "}"
+
+
+def np_array_to_matlab(arr,nfor='{:1.0f}'):
+    """
+    :type arr: np.array
+    :param arr: the array that should be converted to a matlab string
+    :return: the matlab string
+    """
+    arr = np.array(arr)
+
+    if arr.ndim == 2:
+        rowlist = []
+
+        for row in arr:
+            a = " ".join([nfor.format(i) for i in row])
+            rowlist.append( " " + a + " ")
+
+        matrix = ";\n".join(rowlist)
+
+        return "[" + matrix + "]"
+    else: # arr.ndim == 1
+        a = " ".join([nfor.format(i) for i in arr])
+        return "[" + a + "]"
